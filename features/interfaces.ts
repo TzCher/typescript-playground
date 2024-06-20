a3ea16a9ad25a90bc80a184e2e947d9389b58cf7
Tzvetozar Cherkezov <tzcherkezov@outlook.com>
2024-06-20 17:13:02 +0300

    feat: Refactor to use generic interface

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -18,14 +18,45 @@ printVehicle(oldCivic);
 
 interface Vehicle {
   name: string;
-  year: number;
+  year: Date;
   broken: boolean;
+  summary(): string;
+};
+
+const newCivic = {
+  name: 'civic',
+  year: new Date(),
+  broken: false,
+  summary(): string {
+    return `Name: ${this.name}`;
+  }
 };
 
 const printVehicleWithInterface = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken: ${vehicle.broken}`);
+  console.log(vehicle.summary());
 };
 
-printVehicleWithInterface(oldCivic);
+printVehicleWithInterface(newCivic);
+
+// Refactor to be more generic
+
+interface Reportable {
+  summary(): string;
+};
+
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
+};
+
+printSummary(newCivic);
+
+const colaDrink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My Cola drink has ${this.sugar} grams of sugar`;
+  }
+};
+
+printSummary(colaDrink);
