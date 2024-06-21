877bbfb69a418fc3a0f8c676d73c71a1820953da
Tzvetozar Cherkezov <tzcherkezov@outlook.com>
2024-06-21 19:39:04 +0300

    feat: Add Google Maps API

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,10 @@
-console.log('hi there!');
+/// <reference types="@types/google.maps" />
+import { User } from './User.ts';
+import { Company } from './Company.ts';
+
+const user = new User();
+console.log(user);
+
+const company = new Company();
+console.log(company);
+
