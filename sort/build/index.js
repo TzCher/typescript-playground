635f2def119d3291bebe32390d4e5a1ad0d0e63e
Tzvetozar Cherkezov <tzcherkezov@outlook.com>
2024-06-22 10:54:44 +0300

    feat: Initial commit and config

---
+++ sort/build/index.js
@@ -0,0 +1,12 @@
+"use strict";
+const add = (a, b) => a + b;
+add(1, 2); // 3
+const subtract = (a, b) => a - b;
+const res = subtract(2, 1); // 1
+console.log(res); // 1
+const divide = (a, b) => a / b;
+const res2 = divide(4, 2); // 2
+console.log(res2); // 2
+const multiply = (a, b) => a * b;
+const res3 = multiply(2, 3); // 6
+console.log(res3); // 6
---
+++ sort/src/index.ts
@@ -0,0 +1,22 @@
+const add = (a: number, b: number): number => a + b;
+
+add(1, 2); // 3
+
+
+const subtract = (a: number, b: number): number => a - b;
+
+const res = subtract(2, 1); // 1
+
+console.log(res); // 1
+
+const divide = (a: number, b: number): number => a / b;
+
+const res2 = divide(4, 2); // 2
+
+console.log(res2); // 2
+
+const multiply = (a: number, b: number): number => a * b;
+
+const res3 = multiply(2, 3); // 6
+
+console.log(res3); // 6
