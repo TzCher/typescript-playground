b56234c02697b767674f32d4f6dd04fc60268c59
Tzvetozar Cherkezov <tzcherkezov@outlook.com>
2024-06-22 14:41:15 +0300

    fix: Refactor to extract compare and swap logic

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,35 +1,8 @@
 "use strict";
-class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
-    sort() {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const temp = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = temp;
-                    }
-                }
-                if (typeof this.collection === 'string') {
-                    const strArr = this.collection.split('');
-                    if (strArr[j] > strArr[j + 1]) {
-                        const temp = strArr[j];
-                        strArr[j] = strArr[j + 1];
-                        strArr[j + 1] = temp;
-                    }
-                    this.collection = strArr.join('');
-                }
-            }
-        }
-    }
-}
-const sorter = new Sorter([10, 3, -5, 0]);
+Object.defineProperty(exports, "__esModule", { value: true });
+const Sorter_1 = require("./Sorter");
+const NumbersCollection_1 = require("./NumbersCollection");
+const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(sorter.collection);
-const stringSorter = new Sorter('Xaayb');
-stringSorter.sort();
-console.log(stringSorter.collection);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,36 +1,9 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
 
-  sort(): void {
-    const { length } = this.collection;
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const temp = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = temp;
-          }
-        }
-
-        if (typeof this.collection === 'string') {
-          const strArr = this.collection.split('');
-          if (strArr[j] > strArr[j + 1]) {
-            const temp = strArr[j];
-            strArr[j] = strArr[j + 1];
-            strArr[j + 1] = temp;
-          }
-          this.collection = strArr.join('');
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(sorter.collection);
-const stringSorter = new Sorter('Xaayb');
-stringSorter.sort();
-console.log(stringSorter.collection);
+
