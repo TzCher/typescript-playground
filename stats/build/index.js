190118711b4d06857e000071287cc4d96ed4f052
Tzvetozar Cherkezov <tzcherkezov@outlook.com>
2024-06-23 23:21:55 +0300

    fix: Refactor to use abstract class and generics

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,10 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const CSVFileReader_1 = require("./CSVFileReader");
+const MatchReader_1 = require("./MatchReader");
 const MatchResult_1 = require("./MatchResult");
-const reader = new CSVFileReader_1.CSVFileReader('football.csv');
+const reader = new MatchReader_1.MatchReader('football.csv');
 reader.read();
-const dateOfFirstMatch = reader.data[0][0];
 let manUnitedWins = 0;
 for (let match of reader.data) {
     if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,14 +1,10 @@
-import { CSVFileReader } from "./CSVFileReader";
+import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./MatchResult";
 
-const reader = new CSVFileReader('football.csv');
+const reader = new MatchReader('football.csv');
 reader.read();
 
-const dateOfFirstMatch = reader.data[0][0];
-
-
 let manUnitedWins = 0;
-
 
 for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
