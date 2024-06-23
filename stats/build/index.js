fa2c9a5bb2b16a2bb1ec1a99c17d29b9660df08d
Tzvetozar Cherkezov <tzcherkezov@outlook.com>
2024-06-23 17:02:14 +0300

    feat: Refactor to use enum

--- stats/build/index.js
+++ stats/build/index.js
@@ -8,14 +8,19 @@ const matches = fs_1.default
     .readFileSync('./football.csv', 'utf8')
     .split('\n')
     .map((row) => row.split(','));
-const homeWin = 'H';
-const awayWin = 'A';
+var matchResult;
+(function (matchResult) {
+    matchResult["HomeWin"] = "H";
+    matchResult["AwayWin"] = "A";
+    matchResult["Draw"] = "D";
+})(matchResult || (matchResult = {}));
+;
 let manUnitedWins = 0;
 for (let match of matches) {
-    if (match[1] === 'Man United' && match[5] === homeWin) {
+    if (match[1] === 'Man United' && match[5] === matchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === 'Man United' && match[5] === awayWin) {
+    else if (match[2] === 'Man United' && match[5] === matchResult.AwayWin) {
         manUnitedWins++;
     }
 }
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -5,14 +5,18 @@ const matches = fs
   .split('\n')
   .map((row: string): string[] => row.split(','));
 
-const homeWin = 'H';
-const awayWin = 'A';
+enum matchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+};
+
 let manUnitedWins = 0;
 
 for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === homeWin) {
+  if (match[1] === 'Man United' && match[5] === matchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === awayWin) {
+  } else if (match[2] === 'Man United' && match[5] === matchResult.AwayWin ) {
     manUnitedWins++;
   }
 }
