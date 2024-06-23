0e5333c860d0e1c9c8aa168d230e6fc671eb657e
Tzvetozar Cherkezov <tzcherkezov@outlook.com>
2024-06-23 23:46:49 +0300

    fix: Second refactor with interface

--- stats/build/inheritance/CSVFileReader.js
+++ stats/build/inheritance/CSVFileReader.js
@@ -3,9 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.CsvFileReader = void 0;
+exports.CSVFileReader = void 0;
 const fs_1 = __importDefault(require("fs"));
-class CsvFileReader {
+class CSVFileReader {
     constructor(filename) {
         this.filename = filename;
         this.data = [];
@@ -18,7 +18,8 @@ class CsvFileReader {
             .split('\n')
             .map((row) => {
             return row.split(',');
-        });
+        })
+            .map(this.mapRow);
     }
 }
-exports.CsvFileReader = CsvFileReader;
+exports.CSVFileReader = CSVFileReader;
--- stats/src/MatchReader.ts
+++ stats/src/MatchReader.ts
@@ -1,20 +1,31 @@
-import { CSVFileReader } from "./CSVFileReader";
-import { MatchResult } from "./MatchResult";
-import { dateStringToDate } from "./utils";
+import { dateStringToDate } from './utils';
+import { MatchResult } from './MatchResult';
+
+interface DataReader { 
+  read(): void;
+  data: string[][];
+}
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-export class MatchReader extends CSVFileReader<MatchData> {
+export class MatchReader {
 
-  mapRow(row: string[]): MatchData {
-    return [
-      dateStringToDate(row[0]),
-      row[1], 
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult,
-      row[6],
-    ];
+  matches: MatchData[] = [];
+
+  constructor(public reader: DataReader) {}
+
+  load(): void {
+    this.reader.read();
+    this.matches = this.reader.data.map((row: string[]): MatchData => {
+      return [
+        dateStringToDate(row[0]),
+        row[1], 
+        row[2],
+        parseInt(row[3]),
+        parseInt(row[4]),
+        row[5] as MatchResult,
+        row[6],
+      ];
+    });
   }
 }
