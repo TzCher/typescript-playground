f3cbb64bbbc8d311186c54e5d889258ea9a39bd3
Tzvetozar Cherkezov <tzcherkezov@outlook.com>
2024-06-24 13:15:21 +0300

    feat: Add html report support

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -3,6 +3,7 @@ import { CsvFileReader } from "./CsvFileReader";
 import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { Summary } from "./Summary";
 import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { HtmlReport } from "./reportTargets/HtmlReport";
 
 const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
@@ -11,3 +12,6 @@ matchReader.load();
 const manUnitedWins = new WinsAnalysis('Man United');
 const summary = new Summary(manUnitedWins, new ConsoleReport());
 summary.buildAndPrintReport(matchReader.matches);
+
+const htmlSummary = new Summary(manUnitedWins, new HtmlReport());
+htmlSummary.buildAndPrintReport(matchReader.matches);
