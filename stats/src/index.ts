346945077a7007b9e58d201eaaed30af0d807d7c
Tzvetozar Cherkezov <tzcherkezov@outlook.com>
2024-06-24 13:02:28 +0300

    fix: Refactored with true composition approach

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,20 +1,13 @@
 import { MatchReader } from "./MatchReader";
-import { MatchResult } from "./MatchResult";
 import { CsvFileReader } from "./CsvFileReader";
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
+import { Summary } from "./Summary";
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
 
 const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
-
 matchReader.load();
 
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
+const manUnitedWins = new WinsAnalysis('Man United');
+const summary = new Summary(manUnitedWins, new ConsoleReport());
+summary.buildAndPrintReport(matchReader.matches);
