4cf3d2b9d30b315161494626d2ab0f0b54ee35ae
Tzvetozar Cherkezov <tzcherkezov@outlook.com>
2024-07-13 20:09:43 +0300

    feat: Add json-server

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,13 +2,21 @@ import { User } from "./models/User";
 
 const user = new User({ name: "John", age: 30 });
 
-user.set({ name: "newname", age: 9999});
+user.on("change", () => {
+  console.log("User was changed");
+});
 
-user.set({ name: "newname"});
+user.on("change", () => {
+  console.log("User was changed 2");
+});
 
-user.set({ age: 9999});
+user.on("save", () => {
+  console.log("User was saved");
+});
 
-user.set({ name: "Jane", age: 24});
+user.on("save", () => {
+  console.log("User was saved 2");
+});
 
-console.log(user.get("name"));
-console.log(user.get("age"));
+user.trigger("change");
+user.trigger("save");
