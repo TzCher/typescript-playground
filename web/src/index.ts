8ef2e886b9f8c76e76b4c5d8aa2bc5e74c4e5446
Tzvetozar Cherkezov <tzcherkezov@outlook.com>
2024-07-13 18:46:28 +0300

    feat: Add optional UserProps and Callback type alias

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello, world!');
+import { User } from "./models/User";
+
+const user = new User({ name: "John", age: 30 });
+
+user.set({ name: "newname", age: 9999});
+
+user.set({ name: "newname"});
+
+user.set({ age: 9999});
+
+user.set({ name: "Jane", age: 24});
+
+console.log(user.get("name"));
+console.log(user.get("age"));
